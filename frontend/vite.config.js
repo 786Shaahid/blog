23430a0e252614f67a6c876cbb58dbfcae46fdf2
Shahid Raza <shahidraza02qwert@gmail.com>
2024-06-24 02:13:26 +0530

    modify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,13 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {
-  resolve
-} from "path";
-import {compression} from 'vite-plugin-compression2'
+
+
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(),
-    splitVendorChunkPlugin(),
-    compression({
-      algorithm: 'brotliCompress'
-    })],
+  plugins: [react()],
   server: {
     proxy: {
       "/api/v1": {
@@ -24,32 +18,15 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      output: {
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-
-        assetFileNames: ({ name }) => {
-          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
-            return 'assets/images/[name]-[hash][extname]';
-          }
-          if (/\.css$/.test(name ?? '')) {
-            return 'assets/css/[name]-[hash][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
-        }
-      },
-     
+            external: [
+        "react",
+        "react-dom",
+        "react-router-dom",
+        "axios"],
     },
     reportCompressedSize: true,
    sourcemap: true
   },
-resolve: {
-  alias: {
-    '@components': resolve(__dirname, 'src/components'),
-    '@utils': resolve(__dirname, 'src/utils'),
-    '@src': resolve(__dirname, 'src'),
-    '@manager':resolve(__dirname,'src/manager')
-  },
-},
+
 
 })
