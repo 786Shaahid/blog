20f3b5be8f6fc4527ef5314f0d4946eccba6ddae
Shahid Raza <shahidraza02qwert@gmail.com>
2024-06-23 20:18:45 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {
+  resolve
+} from "path";
 
+const aliases = {
+  '@crema': 'src/@crema',
+  'core': 'src/core',
+  'assets': 'src/assets',
+  '@hook': 'src/@hook',
+  'components': 'src/components',
+  'features': 'src/features',
+  'guards': 'src/guards',
+  'pages': 'src/pages',
+  'types': 'src/types',
+};
+
+const resolvedAliases = Object.fromEntries(
+  Object.entries(aliases).map(([key, value]) => [key, resolve(__dirname, value)]),
+);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -12,4 +30,21 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+        external: [
+            "react",
+            "react-dom",
+            "react-router-dom"
+        ],
+    }
+},
+resolve: {
+  alias: {
+      ...resolvedAliases,
+      './runtimeConfig': './runtimeConfig.browser',
+      'jss-plugin-{}': 'jss-plugin-global'
+  },
+},
+
 })
